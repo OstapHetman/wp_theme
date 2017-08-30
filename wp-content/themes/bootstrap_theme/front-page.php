362ed1c2cd62f1c71d7d06baec43fd8339997313
OstapHetman <prickly4528@gmail.com>
2017-08-30 16:49:45 +0300

    implement front page in wp

--- wp-content/themes/bootstrap_theme/front-page.php
+++ wp-content/themes/bootstrap_theme/front-page.php
@@ -43,9 +43,46 @@
             </nav>
         </div>
     </div>
-    <div class="container">
 
-        <div class="blog-header">
-            <h1 class="blog-title"><?php bloginfo('name');?></h1>
-            <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
-        </div>
+<section class="showcase">
+        <div class="container">
+             <h1>Custom Bootstrap Wordpress Theme</h1>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, at. Debitis ullam odio, veritatis quos neque libero soluta consequatur quis eos accusantium dolor atque unde reprehenderit commodi ad, tenetur officiis.</p>
+             <a href="" class="btn btn-primary btn-lg">Read More..</a>
+         </div>
+     </section>
+ 
+     <section class="boxes">
+         <div class="container">
+             <div class="row">
+                 <div class="col-md-4">
+                 <?php if(is_active_sidebar('box1')) : ?>
+                 <?php dynamic_sidebar('box1'); ?>
+               <?php endif; ?>
+                 </div>
+                 <div class="col-md-4">
+                 <?php if(is_active_sidebar('box2')) : ?>
+                 <?php dynamic_sidebar('box2'); ?>
+               <?php endif; ?>
+                 </div>
+                 <div class="col-md-4">
+                 <?php if(is_active_sidebar('box3')) : ?>
+            <?php dynamic_sidebar('box3'); ?>
+          <?php endif; ?>
+                </div>
+             </div>
+         </div>
+     </section>
+
+     <footer class="blog-footer">
+        <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
+        <p>
+            <a href="#">Back to top</a>
+        </p>
+    </footer>
+            <?php wp_footer(); ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+</body>
+
+</html>
