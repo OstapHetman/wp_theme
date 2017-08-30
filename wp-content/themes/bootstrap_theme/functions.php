6ecae793fbfd96d78acf77ed6fa1908e1d3e78da
OstapHetman <prickly4528@gmail.com>
2017-08-30 14:50:01 +0300

    implement main post loop

--- wp-content/themes/bootstrap_theme/functions.php
+++ wp-content/themes/bootstrap_theme/functions.php
@@ -4,9 +4,18 @@ require_once('wp-bootstrap-navwalker.php');
 
 // Theme Support
 function wpb_theme_setup() {
+    add_theme_support('post-thumbnails');
     // Nav Menu
     register_nav_menus(array (
         'primary' => _('Primary Menu')
     ));
 }
 add_action('after_setup_theme', 'wpb_theme_setup');
+
+// Excerpt Length Control
+
+function set_excerpt_length() {
+    return 40;
+}
+
+add_filter('excerpt_length', 'set_excerpt_length');
